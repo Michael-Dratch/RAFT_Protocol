c8b1b4ae4cf3cb78fdb5f39f2381f882d7a0fad9
Michael Dratch <michael.s.dratch@gmail.com>
2023-05-26 08:48:00 -0400

    Initial Commit

---
+++ RaftMessenger.h
@@ -0,0 +1,14 @@
+//
+// Created by Owner on 5/26/2023.
+//
+
+#ifndef RAFT_PROTOCOL_RAFTMESSENGER_H
+#define RAFT_PROTOCOL_RAFTMESSENGER_H
+
+
+class RaftMessenger {
+
+};
+
+
+#endif //RAFT_PROTOCOL_RAFTMESSENGER_H
